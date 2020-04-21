784a7beeb0f68a57c28ab293df0df71246038d62
Zulmianah <40556052+zulmianah@users.noreply.github.com>
2020-04-21 18:28:55 +0300

    Create sleep.php

---
+++ SingleFile-master/cli/sleep.php
@@ -0,0 +1,12 @@
+<?php
+require 'single-file.php';
+date_default_timezone_set("Africa/Nairobi");
+date_default_timezone_get();
+$start = time();
+$cmd = 'single-file https://www.alacase.fr/les-avantages-du-service-traiteur-pour-une-reception/ ../../my-single-file-website/www.alacase.fr/les-avantages-du-service-traiteur-pour-une-reception.html';
+	// sleep(30);
+execInBackground($cmd);
+$finnish = time();
+$interval = ($finnish - $start)/60;
+var_dump($interval);
+?>
